d7e45aee61b7b92b09f0b5e5876cda04d28901f1
Layton4 <Javierete4774@gmail.com>
2021-11-26 13:58:33 +0100

    codigo spawn a medias

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -7,6 +7,10 @@ public class SpawnManager : MonoBehaviour
 
     public GameObject Obstacleprefab;
     private float xlim = 200;
+    private float ylim = 200;
+    private float ylim_inf = 0;
+    private float zlim = 200;
+    private Vector3 SpawnPosition = new Vector3(0, 0, 30);
 
 
     // Start is called before the first frame update
